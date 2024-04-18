a0ef23b3a4703879a8c7018dffbdf848e25997ef
Paul Müller <dev@craban.de>
2024-04-18 19:19:03 +0200

    basic tests

--- win_test.sh
+++ win_test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # This script is run as part of the CI pipeline to test whether
 # the binaries created with pyinstaller work.
+set -e
+set -x
+
+# Test whether we can print the version
 ./dist/chipstream-cli --version
 ./dist/dclab-compress --version
 ./dist/dclab-verify-dataset --version
