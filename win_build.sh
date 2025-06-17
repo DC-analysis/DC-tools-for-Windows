0fe07f3a82a5be34f1baeca77a623f71d8a2b009
Paul Müller <dev@craban.de>
2025-06-17 18:44:54 +0200

    build: add CUDA executables

--- win_build.sh
+++ win_build.sh
@@ -17,9 +17,6 @@ python3 -m pip install --upgrade pip wheel
 python3 -m pip install --upgrade pyinstaller
 python3 -m pip install msvc-runtime
 
-# Install CPU versions of torch (smaller file size)
-pip install torch==2.3.1+cpu torchvision==0.18.1+cpu -f https://download.pytorch.org/whl/torch_stable.html
-
 # upgrade packages
 pip install -r requirements.txt
 
